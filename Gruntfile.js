7f4b10dcd8ca91bbbee826e7a16d0050e30bb507
Krzysztof Nowak <mail@krzysztofnowak.net>
2015-07-11 00:22:16 +0200

    grunt functionality adjusted to properly test it

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,66 +8,71 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/*.js',
+				'<%= nodeunit.tests %>'
+			],
+			options: {
+				jshintrc: '.jshintrc'
+			}
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
 
-    // Configuration to be run (and then tested).
-    version_and_label: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/package.json']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/package.json']
-        }
-      }
-    },
+		copy: {
+			tests: {
+				expand: true,
+				flatten: true,
+				src: 'test/pattern/**',
+				dest: 'test/tmp/',
+				filter: 'isFile'
+			}
+		},
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+		// Configuration to be run (and then tested).
+		version_and_label: {
+			default_options: {},
+			custom_options: {
+				options: {
+					dir: 'test/tmp/'
+				},
+				files: {
+					'tmp/custom_options': ['test/fixtures/index.htm', 'test/fixtures/package.json']
+				}
+			}
+		},
 
-  });
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		}
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+	});
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'version_and_label', 'nodeunit']);
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	//grunt.registerTask('test', ['clean', 'copy', 'version_and_label', 'nodeunit']);
+	grunt.registerTask('test', ['copy', 'version_and_label']);
+
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'test']);
 
 };
--- tasks/version_and_label.js
+++ tasks/version_and_label.js
@@ -15,46 +15,50 @@ module.exports = function(grunt) {
 
 	var buildNumber;
 
+
   grunt.registerMultiTask('version_and_label', 'Grunt task to manage versioning and displaying on page', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+	    dir: '',
+      pkg: 'package.json'
     });
 
+	  var filepathIn = options.dir+options.pkg;
+	  var filepathOut = options.dir+options.pkg;
     // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
+    //options.pkg.forEach(function(f) {
+    //  // Handle reading file
+    //  var src = f.src.filter(function(filepath) {
         // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
+        if (!grunt.file.exists(filepathIn)) {
+          grunt.log.warn('Source file "' + filepathIn + '" not found.');
           return false;
-        } else {
-          return true;
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
+      //}).map(function(filepath) {
+      //  // Read file source.
+      //  return grunt.file.read(filepath);
+      //});
+    //
+	  var src = grunt.file.read(filepathIn);
+	   // // Convert to object, find build and increment number
+	   var inputConfig = JSON.parse(src);
+	   buildNumber = inputConfig.build;
+    grunt.log.warn('file: >>' + buildNumber+'<<');
 
-	    var inputConfig = JSON.parse(src);
-	    buildNumber = inputConfig.build;
+	   buildNumber += 1;
 
-	    buildNumber += 1;
+	   inputConfig.build = buildNumber;
 
-	    inputConfig.build = buildNumber;
+	   var modifiedInputConfig = JSON.stringify(inputConfig, null, '\t');
 
-	    var modifiedInputConfig = JSON.stringify(inputConfig, null, '\t');
-
-	    grunt.log.writeln('test: '+ buildNumber);
+	   grunt.log.writeln('test: '+ buildNumber);
 
       // Write the destination file.
-      grunt.file.write(f.dest, modifiedInputConfig);
+      grunt.file.write(filepathOut, modifiedInputConfig);
 
       // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });
+      grunt.log.writeln('File "' + filepathOut + '" updated.');
+    //});
   });
 
 };
